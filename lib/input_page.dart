f0615e1c0ca972d277ae6d88bb6cfb1b09198397
Venali Soni <sonivenali.vs@gmail.com>
2021-04-27 21:51:16 +0530

    Created Weight And Age Design

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +7,10 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  String selected = "";
+  int weight = 30;
+  int age = 30;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,42 +25,178 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: [
                 Expanded(
-                    child: Container(
-                  margin: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)),
-                )),
-                Expanded(child: Container(
-                  margin: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)),
-                )),
+                  child: buildBox(
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80,
+                      ),
+                      "MALE"),
+                ),
+                Expanded(
+                    child: buildBox(
+                        Icon(
+                          FontAwesomeIcons.venus,
+                          size: 80,
+                        ),
+                        "FEMALE")),
               ],
             )),
-            Expanded(child: Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10)),
-            )),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: Color(0xFF111328),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
             Expanded(
                 child: Row(children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10)),
-              )),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10)),
-              )),
+              Expanded(child: buildWeight()),
+              Expanded(child: buildAge())
             ])),
+            Container(
+              child: Center(
+                  child: Text(
+                "CALCULATE",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              )),
+              color: Color(0xFFEB1555),
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              height: 80,
+            )
           ],
         ));
+  }
+
+  Widget buildWeight() {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: Color(0xFF111328), borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "WEIGHT",
+            style: TextStyle(
+                color: Color(
+                  0xFF8D8E98,
+                ),
+                fontSize: 18),
+          ),
+          Text(weight.toString()),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Color(0xFF8D8E98),
+                    child: Icon(Icons.remove, size: 15),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                ),
+                Expanded(
+                  child: RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Color(0xFF8D8E98),
+                    child: Icon(Icons.add, size: 15),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildAge() {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: Color(0xFF111328), borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "AGE",
+            style: TextStyle(
+                color: Color(
+                  0xFF8D8E98,
+                ),
+                fontSize: 18),
+          ),
+          Text(age.toString()),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Color(0xFF8D8E98),
+                    child: Icon(Icons.remove, size: 15),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                ),
+                Expanded(
+                  child: RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Color(0xFF8D8E98),
+                    child: Icon(Icons.add, size: 15),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildBox(icon, String label) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = label;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: Color(selected == label ? 0xFF1D1E33 : 0xFF111328),
+            borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                  color: Color(
+                    0xFF8D8E98,
+                  ),
+                  fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
